f61c0c1c79b97769a1218748758a3acde048f382
Byambatsogt Tumurkhuu <pcetsogtoo@gmail.com>
2012-06-13 09:22:10 -0500

    serializer test pass

--- src/test/scala/serializer.scala
+++ src/test/scala/serializer.scala
@@ -45,37 +45,21 @@ class SerializerSpec extends Specification {
 
   def isEqual(m1: Mail, m2: Mail): Boolean = {
 
-    println(m1.getName == m2.getName)
-    println(m1.getRemoteHost == m2.getRemoteHost )
-    println(m1.getRemoteAddr == m2.getRemoteAddr ) 
-    println(m1.getRecipients == m2.getRecipients )
-    println(m1.getSender == m2.getSender )
-    println(m1.getMessage.getSubject == m2.getMessage.getSubject)
-    println(m1.getMessage.getFrom.toList == m2.getMessage.getFrom.toList )
-    println(m1.getMessage.getContent == m2.getMessage.getContent)
-    println(m1.getMessage == m2.getMessage)
-
-    println(m1.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(0))
-    println(m1.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(1))
-    println(m2.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(0))
-    println(m2.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(1))
-
     val i1 = m1.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(1).getInputStream
     val i2 = m2.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(1).getInputStream
+    val txt1 = m1.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(0).getContent
+    val txt2 = m2.getMessage.getContent.asInstanceOf[MimeMultipart].getBodyPart(0).getContent
 
-    val (b1, b2) = (IOUtils.toByteArray(i1), IOUtils.toByteArray(i2))
-    println(i1 == i2)
-    println(b1.toList == b2.toList)
-    println(IOUtils.contentEquals(i1, i2))
-
-    m1.getName                       == m2.getName &&
-    m1.getRemoteHost                 == m2.getRemoteHost &&
-    m1.getRemoteAddr                 == m2.getRemoteAddr && 
-    m1.getSender                     == m2.getSender &&
-    m1.getMessage.getSubject         == m2.getMessage.getSubject &&
-    m1.getRecipients                 == m2.getRecipients &&
-    m1.getMessage.getFrom.toList     == m2.getMessage.getFrom.toList &&
-    m1.getMessage.getContent         == m2.getMessage.getContent
+    m1.getName                              == m2.getName &&
+    m1.getRemoteHost                        == m2.getRemoteHost &&
+    m1.getRemoteAddr                        == m2.getRemoteAddr && 
+    m1.getSender                            == m2.getSender &&
+    m1.getMessage.getSubject                == m2.getMessage.getSubject &&
+    m1.getRecipients                        == m2.getRecipients &&
+    m1.getMessage.getFrom.toList            == m2.getMessage.getFrom.toList &&
+    m1.getMessage.getAllRecipients.toList   == m2.getMessage.getAllRecipients.toList &&
+    txt1 == txt2 &&
+    IOUtils.contentEquals(i1, i2)
   }
 
   "SerializerSpec test" should {
@@ -87,7 +71,6 @@ class SerializerSpec extends Specification {
       m1.setMessage(message)
 
       val bytes = serializer.serialize(m1)
-
       val m2 = serializer.deserialize(bytes)
 
       isEqual(m1, m2) must_== true
