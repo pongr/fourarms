8d8892b550495a315c3338f9c4a227e275c6d103
Byambatsogt Tumurkhuu <pcetsogtoo@gmail.com>
2012-06-13 23:04:45 -0500

    simpledb query update, use count instead of select all

--- src/main/scala/matcher/simpledb-lookup.scala
+++ src/main/scala/matcher/simpledb-lookup.scala
@@ -2,12 +2,12 @@ package com.pongr.fourarms.matcher
 
 import com.amazonaws.services.simpledb.AmazonSimpleDBClient
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.services.simpledb.model.Item
+import com.amazonaws.services.simpledb.AmazonSimpleDB
+import com.amazonaws.services.simpledb.model.{SelectRequest, Item}
 
-import com.pongr.fourarms.simpledb._
-import com.pongr.fourarms.simpledb.Implicits._
+import scala.collection.JavaConversions._
 
-trait SimpleDbLookup extends Lookup with SimpleDbQuery {
+trait SimpleDbLookup extends Lookup {
 
   // SimpleDB credentials
   def accessKeyId: String
@@ -18,10 +18,14 @@ trait SimpleDbLookup extends Lookup with SimpleDbQuery {
   // Creating an instance of the email lookup
   lazy val sdb = new AmazonSimpleDBClient(new BasicAWSCredentials(accessKeyId, secretAccessKey))
 
-  def query = "select * from %s where %s is not null" format (domain, attribute)
-  def itemToString(item: Item): String = item(attribute) getOrElse ""
-  def items: Seq[String] = selectMany(sdb) (itemToString) (query)
+  def query(value: String) = "select count(*) from %s where %s='%s'" format (domain, attribute, value)
 
-  def exist_?(e: String) = items contains e
+  def exist_?(e: String) = {
+    val selectRequest = new SelectRequest(query(e))
+    sdb.select(selectRequest).getItems.headOption map { item =>
+      // Item = {Name: Domain, Attributes: [{Name: Count, Value: 1, }], }
+      item.getAttributes.headOption.map { _.getValue != "0" } getOrElse false
+    } getOrElse false
+  }
 
 }
